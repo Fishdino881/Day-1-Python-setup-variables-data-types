639ee22004d9742e0f642769ea1b480a55724364
Fishdino881 <i.kunalsingh.ks@gmail.com>
2026-01-06 23:25:16 +0530

    Add files via upload

---
+++ Day 1 – Python setup, variables, data types.py
@@ -0,0 +1,39 @@
+# Integer type
+age = 21
+players = 2
+quantity = 5
+
+print(f"I am {age} years old")
+print(f"We are {players} players")
+print(f"I have {quantity} items")
+
+# Float type
+gpa = 3.2
+distance = 2.4
+price = 10.99
+
+print(f"My GPA is {gpa}")
+print(f"My house distance is {distance} km")
+print(f"The iPhone price is {price}")
+
+# String type
+name = "Kunal"
+food = "Pizza"
+email = "ikunalsinghks@gmail.com"
+
+print(f"My name is {name}")
+print(f"My favorite food is {food}")
+print(f"My email ID is {email}")
+
+# Boolean type
+study = True
+not_study = False
+
+print(f"Am I studying? {study}")
+print(f"Am I not studying? {not_study}")
+
+# Boolean in condition (important)
+if study:
+    print("I am studying Python")
+else:
+    print("I am not studying")
